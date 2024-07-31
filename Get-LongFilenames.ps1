ca0fba95cdde71c31fdc8f51df276d0225d9b28c
tigerguppy <github@tigerguppy.com>
2024-07-31 10:40:14 -0500

    Update Get-LongFilenames.ps1
    Added comments, cleaned up a few ambiguities.

--- Get-LongFilenames.ps1
+++ Get-LongFilenames.ps1
@@ -1,14 +1,17 @@
-$Path = 'E:\'
+# Where to search for long file names.
+$SearchPath = 'E:\'
+
+# Max lenght of path. Add items longer than or equal to this number to the output report.
 $MaxPathLen = 250
 
 # How many files to check before updating the status. 
 # Don't set too low (below 100 or so) or it'll impact performance.
-$FileQtyBeforeUpdateStatus = 100 
+$FileQtyBeforeUpdateStatus = 100
+
+# Where to save the output files.
 $LogPath = 'C:\Temp'
 
 # Do not edit below this line #
-$LogTime = $(Get-Date -f yyyyMMdd-HHmmss)
-$ScriptLogPath = Join-Path -Path $LogPath -ChildPath "$LogTime-LongFilesTranscript.txt" -ErrorAction Stop
 
 function Start-Logging {
 
@@ -33,32 +36,44 @@ function Stop-Logging {
 
 }
 
+# Get current date & time and setup log file.
+$LogTime = $(Get-Date -f yyyyMMdd-HHmmss)
+$ScriptLogPath = Join-Path -Path $LogPath -ChildPath "$LogTime-LongFilesTranscript.txt" -ErrorAction Stop
+
 Start-Logging
 
-Write-Output "$(Get-Date -Format O) : Getting files from $Path. This may take a while."
+Write-Output "$(Get-Date -Format O) : Getting files from $SearchPath. This may take a while."
 
-$AllItems = Get-ChildItem -Path "$Path" -Recurse -Force -ErrorAction SilentlyContinue
+# Get all the files in the search path.
+$AllItems = Get-ChildItem -Path "$SearchPath" -Recurse -Force -ErrorAction SilentlyContinue
 
-$AllItemsQty = $($AllItems | Measure-Object).Count
+# Get qty of items to check.
+$AllItemsQty = $AllItems.Count
 
 Write-Output "$(Get-Date -Format O) : Found $AllItemsQty items."
 
+# Counters for status updates
 $CurrentCheck = 0
 $FoundLongPaths = 0
 
+# Where to hold the flagged long paths
 [System.Collections.ArrayList]$LongFiles = @{}
 
+# Loop through all the items in the search path to look for long paths.
 foreach ($Item in $AllItems) {
 
+    # Check if status should be updated.
     if ($CurrentCheck % $FileQtyBeforeUpdateStatus -eq 0) {
         $PercentComplete = $($CurrentCheck / $AllItemsQty) * 100
         Write-Progress -Activity 'Finding long file paths.' -Status "Test: $CurrentCheck / $AllItemsQty. Found: $FoundLongPaths" -PercentComplete $PercentComplete
 
     }
 
+    # Get the length of the item
     $ItemLen = $($Item.FullName.Trim()).length
 
-    if ($ItemLen -gt $MaxPathLen) {
+    # If length of the item is longer than the desired length, add it to the output list.
+    if ($ItemLen -ge $MaxPathLen) {
 
         $FoundLongPaths ++
         $LongFiles.Add($Item) | Out-Null
@@ -69,12 +84,14 @@ foreach ($Item in $AllItems) {
 
 }
 
+# Setup output file.
 Write-Output "$(Get-Date -Format O) : Found $FoundLongPaths long path names."
-
 $OutputFilePath = Join-Path -Path $LogPath -ChildPath "$LogTime-LongFiles.txt" -ErrorAction Stop
 
+# Output found long paths to a file.
 Write-Output "$(Get-Date -Format O) : Long file paths exported to $OutputFilePath"
-
 $LongFiles | Out-File -FilePath $OutputFilePath -NoClobber
 
 Stop-Logging
+
+Start-Process -FilePath "$env:SystemRoot\Explorer.exe" -ArgumentList $LogPath
