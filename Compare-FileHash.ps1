7086722bf829563d9e150fcd950444659780bf5f
tigerguppy <github@tigerguppy.com>
2024-08-30 16:29:41 -0500

    Update Compare-FileHash.ps1
    Added compare to hash parameter set

--- Compare-FileHash.ps1
+++ Compare-FileHash.ps1
@@ -4,32 +4,67 @@ function Compare-FileHash {
         [Parameter(
             Mandatory = $true,
             Position = 0,
-            HelpMessage = 'Path 1'
+            ParameterSetName = 'CompareTwoFiles',
+            HelpMessage = 'Path of file 1'
         )]
-        [string] $Path1,
+        [Parameter(
+            Mandatory = $true,
+            Position = 0,
+            ParameterSetName = 'CompareFileToHash',
+            HelpMessage = 'Path of file 1'
+        )]
+        [string] $SourcePath,
         
         # Path of second file
         [Parameter(
             Mandatory = $true,
             Position = 1,
-            HelpMessage = 'Path 2'
+            ParameterSetName = 'CompareTwoFiles',
+            HelpMessage = 'Path of file 2'
         )]
-        [string] $Path2,
+        [string] $ComparePath,
+
+        # Hash to compare
+        [Parameter(
+            Mandatory = $true,
+            Position = 1,
+            ParameterSetName = 'CompareFileToHash',
+            HelpMessage = 'Hash to compare'
+        )]
+        [string] $CompareHash,
 		
         # Record type
         [Parameter(
             Mandatory = $false,
             Position = 2,
+            ParameterSetName = 'CompareTwoFiles',
+            HelpMessage = 'Algorithm type'
+        )]
+        [Parameter(
+            Mandatory = $false,
+            Position = 2,
+            ParameterSetName = 'CompareFileToHash',
             HelpMessage = 'Algorithm type'
         )]
         [ValidateSet('MACTripleDES', 'MD5', 'RIPEMD160', 'SHA1', 'SHA256', 'SHA384', 'SHA512')]
         [string] $Algorithm = 'SHA256'
     )
-    
-    $Hash1 = Get-FileHash -Algorithm $Algorithm -Path "$Path1"
-    $Hash2 = Get-FileHash -Algorithm $Algorithm -Path "$Path2"
 
-    if ($Hash1.Hash -eq $Hash2.Hash) { 
+    switch ($PSCmdlet.ParameterSetName) {
+        'CompareTwoFiles' {
+            $Hash1 = $(Get-FileHash -Algorithm $Algorithm -Path "$SourcePath").Hash
+            $Hash2 = $(Get-FileHash -Algorithm $Algorithm -Path "$ComparePath").Hash
+            break
+        }
+
+        'CompareFileToHash' {
+            $Hash1 = $(Get-FileHash -Algorithm $Algorithm -Path "$SourcePath").Hash
+            $Hash2 = $CompareHash
+        }
+        
+    }
+
+    if ($Hash1 -eq $Hash2) { 
         return $true 
     } else { 
         return $false 
