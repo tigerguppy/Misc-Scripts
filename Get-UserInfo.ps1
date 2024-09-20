8ae6f3c637d6f9fd575aa7e5d749cb15207f0443
tigerguppy <github@tigerguppy.com>
2024-09-20 10:43:06 -0500

    Update Get-UserInfo.ps1

--- Get-UserInfo.ps1
+++ Get-UserInfo.ps1
@@ -4,76 +4,36 @@ Import-Module ActiveDirectory
 
 <#
 .SYNOPSIS
-    This script is meant to easily pull select and unsecure information from Active Directory.
+    Retrieve detailed user information from Active Directory, including attributes like account status, group membership, and password properties.
 
 .DESCRIPTION
-    This script is meant to easily pull select and unsecure information from Active Directory.
+    This script retrieves Active Directory user information based on the provided DisplayName. It can search across multiple domain controllers, return custom attributes, and display user groups and password-related data such as password expiration and lockout status.
+
+.PARAMETER DisplayName
+    The display name of the user(s) to query.
+
+.PARAMETER Servers
+    (Optional) Domain Controller NetBIOS name(s). Default is the discovered domain controller.
+
+.PARAMETER SearchBase
+    (Optional) DistinguishedName for narrowing the search. Defaults to the domain DistinguishedName.
+
+.EXAMPLE
+    Get-UserInfo mike
+    Retrieves all user information for users with "mike" in their display name.
+
+.EXAMPLE
+    Get-UserInfo -DisplayName mike -Servers all
+    Queries all domain controllers for users with "mike" in their display name.
 
 .NOTES
-    Additional information about the function or script.
-    Name: Get-UserInfo
     Author: Tony Burrows
-    Contact: scripts@tigerguppy.com
-
-    VERSION HISTORY
-        Created on 2021-04-19
-        Version 1.0 2021-04-19
-            Initial release
-        Version 1.1 2021-04-20
-            Updated to dynamically pull AD info without hardcoded variables.
-        Version 1.2 2021-04-23
-            Remove DLL option to load AD module. Use this method instead if RSAT isn't installed.
-            https://petertheautomator.com/2020/10/05/use-the-active-directory-powershell-module-without-installing-rsat/
-            Copy ActiveDirectory folder to 'C:\Windows\System32\WindowsPowerShell\v1.0\Modules'
-        Version 1.3 2021-04-23
-            Added a couple attributes to the output.
-            Filtered out admin and test accounts.
-        Version 1.4 2021-05-07
-            Added option to list all users.
-            Added sort by date user was created.
-        Version 1.5 2021-05-11
-            List groups when querying individual user
-		Version 1.5.1 2021-05-27
-			Changed default sort
-		Version 1.5.2 2021-12-11
-			Added IPPhone
-        Version 1.5.3 2022-02-15
-            Add exit command
-        Version 1.5.4 2022-02-16
-            Add description and WhenChanged fields to output.
-		Version 1.5.5 2022-02-28
-			Fixed exit so it does not query AD before exiting.
-        Version 1.6.0 2022-03-02
-            Turn script into a function
-            Add option to not show security groups
-        Version 1.7.0 2022-04-18
-            Change search to return custom PSObject
-            Remove "show security groups" option
-        Version 2.0.0 2023-08-11
-            Add list of user groups for individual user lookups
-            Removed bulk listing option due to restructure of script and changing its overall purpose
-            Changed output from objects to console
-        Version 2.0.1 2023-08-16
-            Add PasswordExpired & SamAccountName to output
-        Version 2.0.2 2023-08-23
-            Add extensionAttributes
-            Changed UserProperties to an ArrayList
-        Version 2.0.3 2024-09-16
-            Add BadPwdCount, LastBadPasswordAttempt, & PwdLastSet attributes
-            Add options to specifiy a domain controller and/or DistinguishedName
-        Version 2.0.4 2024-09-20
-            Add AccountLockoutTime & LockedOut attributes
-
-.EXAMPLE
-    Get user infomation from the primary server for all users with mike in their display name.
-    Get-UserInfo mike
-
-.EXAMPLE
-    Get user infomation from all servers for all users with mike in their display name.
-    Get-UserInfo -DisplayName mike -Servers all
+    Version: 2.0.4 (Updated 2024-09-20)
+    Added account lockout attributes and enhancements to output user properties.
 #>
 
-#Clear-Host
+# Script logic starts here
+
 
 function Get-UserInfo {
     [cmdletbinding()]
