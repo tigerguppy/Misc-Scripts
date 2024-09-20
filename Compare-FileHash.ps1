330b73fadcc43831b1abeabbfb6d102b2116b314
tigerguppy <github@tigerguppy.com>
2024-09-20 10:40:52 -0500

    Update Compare-FileHash.ps1

--- Compare-FileHash.ps1
+++ Compare-FileHash.ps1
@@ -1,3 +1,34 @@
+<#
+.SYNOPSIS
+    Compare file hashes between two files or a file and a known hash to ensure they are identical.
+
+.DESCRIPTION
+    This script calculates the hash of one or two files and compares them. It supports using a known hash for comparison. The script uses the Get-FileHash cmdlet with an algorithm such as SHA256.
+
+.PARAMETER Path1
+    The file path for the first file to be compared.
+
+.PARAMETER Path2
+    (Optional) The file path for the second file to be compared. Omit this parameter if using a known hash.
+
+.PARAMETER KnownHash
+    (Optional) The known hash to compare the first file against. Use this instead of Path2.
+
+.PARAMETER Algorithm
+    (Optional) The hashing algorithm to use (e.g., SHA256, SHA1). Default is SHA256.
+
+.EXAMPLE
+    Compare-FileHash.ps1 -Path1 "C:\file1.txt" -Path2 "C:\file2.txt"
+
+.EXAMPLE
+    Compare-FileHash.ps1 -Path1 "C:\file1.txt" -KnownHash "D7F37A6BC..."
+
+.NOTES
+    Author: Tony Burrows
+    Date: 2024-08-30
+    Version: 1.1
+#>
+
 function Compare-FileHash {
     param (
         # Path of first file
