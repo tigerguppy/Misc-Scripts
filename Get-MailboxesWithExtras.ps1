2f60066a4c44ce308ec3007dedf8d112fed44b56
tigerguppy <github@tigerguppy.com>
2024-09-19 20:01:26 -0500

    Update and rename Get-MailboxesWithForwarding.ps1 to Get-MailboxesWithExtras.ps1
    Add mailbox permission list for non-default permissions.
    Add parameters to limit what is being searched for.

---
+++ Get-MailboxesWithExtras.ps1
@@ -0,0 +1,179 @@
+<#
+.SYNOPSIS
+    Looks for mailboxes with forwarding enabled or mailbox rules with forwarding or mailboxes with extra permissions in Exchange Online.
+.DESCRIPTION
+    Looks for mailboxes with forwarding enabled or mailbox rules with forwarding or mailboxes with extra permissions in Exchange Online.
+.NOTES
+    Assumes ExchangeOnline module is already installed and you're connected to EXO.
+    Connects to Exchange Online, gets a list of all the mailboxes, checks them for admin forwarding or Outlook rule forwarding, logs out of Exchange Online, then lists the results.
+
+    Revision history:
+        2024-09-18
+            Intital release
+        2024-09-19
+            Add mailbox permission list for non-default permissions.
+            Add parameters to limit what is being searched for.
+            
+    To-Do:
+        Run mailboxes in parallel.
+
+.LINK
+    https://tony.support/tools
+
+.EXAMPLE
+    Get-MailboxesWithForwarding -CheckForwardingSmtpAddress
+
+.EXAMPLE
+    Connect-ExchangeOnline
+    $ForwardingItems = Get-MailboxesWithExtras -CheckForwardingSmtpAddress -CheckForwardingAddress -CheckMailboxRules -CheckPermissions
+    $Items | Format-Table -AutoSize -Wrap
+    Disconnect-ExchangeOnline -Confirm:$false
+
+.PARAMETER CheckForwardingSmtpAddress
+    Check mailboxes for ForwardingSmtpAddress.
+
+.PARAMETER CheckForwardingAddress
+    Check mailboxes for ForwardingSmtpAddress.
+
+.PARAMETER CheckMailboxRules
+    Check mailboxes for mailbox rules with forwarding enabled.
+
+.PARAMETER CheckPermissions
+    Check mailboxes for additional permissions.
+
+.AUTHOR
+    Tony Burrows
+    script@tigerguppy.com
+#>
+
+function Get-MailboxesWithExtras {
+    [cmdletbinding()]
+    param (
+        [Parameter(Mandatory = $false, 
+            Position = 0,
+            HelpMessage = 'Check mailboxes for ForwardingSmtpAddress.', 
+            ParameterSetName = 'Default')]
+        [switch]$CheckForwardingSmtpAddress,
+        [Parameter(Mandatory = $false, 
+            Position = 1,
+            HelpMessage = 'Check mailboxes for ForwardingSmtpAddress.', 
+            ParameterSetName = 'Default')]
+        [switch]$CheckForwardingAddress,
+        [Parameter(Mandatory = $false, 
+            Position = 2, 
+            HelpMessage = 'Check mailboxes for mailbox rules with forwarding enabled.', 
+            ParameterSetName = 'Default')]
+        [switch]$CheckMailboxRules,
+        [Parameter(Mandatory = $false, 
+            Position = 3, 
+            HelpMessage = 'Check mailboxes for additional permissions.', 
+            ParameterSetName = 'Default')]
+        [switch]$CheckPermissions = $false
+    )
+
+    # Get mailboxes
+    $Mailboxes = Get-Mailbox -ResultSize:Unlimited
+    $MailboxesQty = $Mailboxes.Count
+    $MailboxesCurrentCount = 0
+
+    # New array for returning objects
+    [System.Collections.ArrayList]$Output = [System.Collections.ArrayList]::new()
+
+    # Walk through each mailbox
+    foreach ($Mailbox in $Mailboxes) {
+
+        if ($CheckForwardingSmtpAddress) {
+            # Check for forwarding set by admins
+            $ForwardingSmtpAddress = $Mailbox.ForwardingSmtpAddress
+            if ($null -eq $ForwardingSmtpAddress) {
+                # ForwardingSmtpAddress not enabled
+
+            } else {
+                $Obj = [PSCustomObject]@{
+                    DisplayName       = $Mailbox.DisplayName
+                    UserPrincipalName = $Mailbox.UserPrincipalName
+                    Type              = 'ForwardingSmtpAddress'
+                    Description       = $ForwardingSmtpAddress
+                }
+                $Output.Add($Obj) | Out-Null
+            }
+        } # End CheckForwardingSmtpAddress
+
+        if ($CheckForwardingAddress) {
+            $ForwardingAddress = $Mailbox.ForwardingAddress
+            if ($null -eq $ForwardingAddress) {
+                # ForwardingAddress not enabled
+
+            } else {
+                $Obj = [PSCustomObject]@{
+                    DisplayName       = $Mailbox.DisplayName
+                    UserPrincipalName = $Mailbox.UserPrincipalName
+                    Type              = 'ForwardingAddress'
+                    Description       = $ForwardingAddress
+                }
+                $Output.Add($Obj) | Out-Null
+            }
+        } # End CheckForwardingAddress
+
+        if ($CheckMailboxRules) {
+            # Get user's mailbox rules
+            $Rules = Get-InboxRule -Mailbox $Mailbox.UserPrincipalName
+            $RulesQty = $Rules.Count
+
+            # If there's mailbox rules, add them to the list
+            if ($RulesQty -ne 0) {   
+                foreach ($Rule in $Rules) {
+                    if ($null -eq $Rule.ForwardAsAttachmentTo -and $null -eq $Rule.ForwardTo) {
+                        # Rule doesn't contain forwarding
+                        continue
+
+                    } else {
+                        $Obj = [PSCustomObject]@{
+                            DisplayName       = $Mailbox.DisplayName
+                            UserPrincipalName = $Mailbox.UserPrincipalName
+                            Type              = 'MailboxRule'
+                            Description       = $Rule.Name
+                        }
+                        $Output.Add($Obj) | Out-Null
+                    }
+                }
+            }
+        }
+
+        if ($CheckPermissions) {
+            # Mailbox permissions that aren't self
+            $MailboxPermissions = Get-MailboxPermission -Identity $Mailbox.UserPrincipalName | Where-Object User -Match '@'
+            foreach ($MailboxPermission in $MailboxPermissions) {
+                $Obj = [PSCustomObject]@{
+                    DisplayName       = $Mailbox.DisplayName
+                    UserPrincipalName = $Mailbox.UserPrincipalName
+                    Type              = 'Permission'
+                    Description       = $MailboxPermission.User
+                }
+                $Output.Add($Obj) | Out-Null
+            }
+        }
+
+        # Update mailboxes progress bar
+        $MailboxesCurrentCount ++
+        $PercentComplete = ($MailboxesCurrentCount / $MailboxesQty)
+        $MailboxProgressParameters = @{
+            Id              = 0
+            Activity        = 'Mailboxes'
+            Status          = "$($PercentComplete.tostring('P')) - $MailboxesCurrentCount / $MailboxesQty - $($Mailbox.UserPrincipalName)"
+            PercentComplete = $PercentComplete * 100
+        }
+        Write-Progress @MailboxProgressParameters
+
+    } # End mailbox loop
+
+    # Function clean up
+    Write-Progress -Id 0 -Activity 'Mailboxes' -Completed
+    return $Output
+} # End Get-MailboxesWithForwarding
+
+Connect-ExchangeOnline
+$ForwardingItems = Get-MailboxesWithExtras -CheckForwardingSmtpAddress -CheckForwardingAddress -CheckMailboxRules -CheckPermissions
+Disconnect-ExchangeOnline -Confirm:$false
+
+$ForwardingItems | Format-Table -AutoSize -Wrap
